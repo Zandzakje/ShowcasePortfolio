1c4c22a167c8609d40807924d1710d778feb6bad
Zandzakje <DamianTomasila@hotmail.com>
2025-12-14 22:57:16 +0100

    YY
    app works locally again

--- showcaseportfolio/src/App.js
+++ showcaseportfolio/src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
